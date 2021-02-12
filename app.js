61fd82a0842b6273a77de1a1f00a3471d6df1830
AbeinemukamaVicent <48617563+Abeinevincent@users.noreply.github.com>
2021-02-12 08:04:12 -0800

    Cool animating counter
    This counter is super cool built using the latest technologies of Bootstrap, HTML, CSS snd JavaScript

---
+++ app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.counter').each(function(){
+        $(this).prop('.counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function(now){
+                $(this).text(Math.ceil(now));
+            }
+        })
+    })
+})
+
+
+
+
+
+
